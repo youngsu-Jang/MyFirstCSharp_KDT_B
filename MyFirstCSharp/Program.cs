de14695655e13aa27c504c58a14c9c0cb144b692
admin <admin@DESKTOP-0FJSL6G>
2022-11-15 13:35:15 +0900

    000

--- MyFirstCSharp/Program.cs
+++ MyFirstCSharp/Program.cs
@@ -8,9 +8,6 @@ namespace MyFirstCSharp
         [STAThread]
         static void Main()
         {
-            // 
-
-
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
